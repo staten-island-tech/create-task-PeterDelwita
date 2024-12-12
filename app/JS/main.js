7aa30287174993840be29b499d9e658450b73738
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-12-12 14:02:09 -0500

    h

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,7 @@
 // Plan:
 // Roll three six-sides dice where you have to bet above, equal to, or below a number to win.
 // How to go about it: To bet, make a form-like table and allow users to input information. The game starts once the form is submitted and once the game finishes, it will show the dice numbers and the total and if the player won or lost.
+// Bettinghistory function: clear card container, use filter to select certain cards (maybe use a function), use forEach to add cards to the screen (bets are stored as objects)
 
 const DOMSelectors = [];
 const bettingHistory = [];
