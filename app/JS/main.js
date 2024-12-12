fba18beed4be165918cbfce6873869d352e5ea12
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-12-12 13:59:47 -0500

    saasdasd

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,5 +1,6 @@
 // Plan:
-// Roll three dice where you have to bet above, equal to, or below a number to win.
-// How to go about it: To bet, make a form-like table
+// Roll three six-sides dice where you have to bet above, equal to, or below a number to win.
+// How to go about it: To bet, make a form-like table and allow users to input information. The game starts once the form is submitted and once the game finishes, it will show the dice numbers and the total and if the player won or lost.
 
+const DOMSelectors = [];
 const bettingHistory = [];
