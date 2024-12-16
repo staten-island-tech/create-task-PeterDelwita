a7c1522a95813981637403b3cd707a592bece31a
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-12-16 14:00:18 -0500

    form, dice, containers

--- create/JS/main.js
+++ create/JS/main.js
@@ -3,5 +3,11 @@
 // How to go about it: To bet, make a form-like table and allow users to input information. The game starts once the form is submitted and once the game finishes, it will show the dice numbers and the total and if the player won or lost.
 // Bettinghistory function: clear card container, use filter to select certain cards by by number, win/loss, and type of bet (maybe use a function), use forEach to add cards to the screen (bets are stored as objects)
 
-const DOMSelectors = [];
+const DOMSelectors = {
+  diceContainer: document.querySelector("dice-container"),
+  historyContainer: document.querySelector("history-container"),
+  btn: document.querySelectorAll("btn"),
+};
 const bettingHistory = [];
+
+function rollDice() {}
