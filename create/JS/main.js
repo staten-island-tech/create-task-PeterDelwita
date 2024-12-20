9f108ab023f77b202f392b17008cda0a93bed24f
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-12-20 13:06:13 -0500

    aaaa

--- create/JS/main.js
+++ create/JS/main.js
@@ -4,10 +4,42 @@
 // Bettinghistory function: clear card container, use filter to select certain cards by by number, win/loss, and type of bet (maybe use a function), use forEach to add cards to the screen (bets are stored as objects)
 
 const DOMSelectors = {
-  diceContainer: document.querySelector("dice-container"),
-  historyContainer: document.querySelector("history-container"),
-  btn: document.querySelectorAll("btn"),
+  diceContainer: document.querySelector(".dice-container"),
+  thresholdButton: document.querySelector(".threshold-options"),
+  firstDie: document.querySelector("#die-1"),
+  secondDie: document.querySelector("#die-2"),
+  thirdDie: document.querySelector("#die-3"),
+  historyContainer: document.querySelector(".history-container"),
+  btn: document.querySelectorAll(".btn"),
+  aboveButton: document.querySelector("#above"),
+  belowButton: document.querySelector("#below"),
+  exactButton: document.querySelector("#exact"),
 };
+
 const bettingHistory = [];
+
+function filterAbove() {
+  DOMSelectors.thresholdButton.innerHTML = "Above";
+}
+
+DOMSelectors.aboveButton.addEventListener("click", function () {
+  filterAbove();
+});
+
+function filterBelow() {
+  DOMSelectors.thresholdButton.innerHTML = "Below";
+}
+
+DOMSelectors.belowButton.addEventListener("click", function () {
+  filterBelow();
+});
+
+function filterExact() {
+  DOMSelectors.thresholdButton.innerHTML = "Exact";
+}
+
+DOMSelectors.exactButton.addEventListener("click", function () {
+  filterExact();
+});
 
 function rollDice() {}
