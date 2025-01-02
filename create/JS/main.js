1d08010dbd2e044518ff18432b11e6be1edf5218
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2025-01-02 14:01:13 -0500

    rolldice

--- create/JS/main.js
+++ create/JS/main.js
@@ -9,6 +9,7 @@ const DOMSelectors = {
   firstDie: document.querySelector("#die-1"),
   secondDie: document.querySelector("#die-2"),
   thirdDie: document.querySelector("#die-3"),
+  form: document.querySelector("form"),
   historyContainer: document.querySelector(".history-container"),
   btn: document.querySelectorAll(".btn"),
   aboveButton: document.querySelector("#above"),
@@ -42,4 +43,26 @@ DOMSelectors.exactButton.addEventListener("click", function () {
   filterExact();
 });
 
-function rollDice() {}
+function rollDice() {
+  (DOMSelectors.firstDie.innerHTML = Math.floor(Math.random() * 6)),
+    (DOMSelectors.secondDie.innerHTML = Math.floor(Math.random() * 6)),
+    (DOMSelectors.thirdDie.innerHTML = Math.floor(Math.random() * 6));
+}
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault(), rollDice();
+});
+
+// // "abcfed" "cabdfed"
+// // Put them in order and compare line by line.
+// function findDiff(x, y) {
+//   let sortX = [...x].sort(); // Iterates through the string and adds each letter to an array. Added to JS in 2015
+//   let sortY = [...y].sort();
+
+//   for(let i = 0; i < sortX.length; i++){ // Selection inside iteration is a good idea
+//     if(sortX[i] != sortY[i]) {
+//       return sortX[i];
+//     }
+//   }
+//   // SortX = Array.from(x).sort()
+// }
